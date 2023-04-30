dbce0873163c04d723c8fbcd039418b26b7b5a9b
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-04-30 01:24:49 +0100

    finalcommit1

--- Develop/script.js
+++ Develop/script.js
@@ -2,16 +2,72 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  return "banana"
-}
+  // password character options
+  var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChar = "0123456789";
+  var specialChar = "!@#$%^&*()?.<\>|=+:;,[-_]";
+  var options = "";
+
+  console.log("You clicked the button!") //confirmation of click
+  //prevent screen from refreshing after click
+  event.preventDefault();
+
+  // user prompts for password criteria
+  var lengthChoice = parseInt(prompt("Choose how many characters you would like your password to be?"))
+  var lowercaseChoice = confirm("Would you like your password to contain Lowercase letters?");
+  var uppercaseChoice = confirm("Would you like your password to contain Uppercase letters?");
+  var numericChoice = confirm("Would you like your password to contain numbers?");
+  var specialChoice = confirm("Would you like your password to contain special characters?");
+  
+
+  // user length validation
+  if (lengthChoice.length==0 || lengthChoice < 8 || lengthChoice > 128) {
+    alert(" You must choose a value between 8 and 128");
+    var lengthChoice = parseInt(prompt("Choose how many characters you would like your password to be?"))
+  }
+
+  // if user denies all password character criteria alert & prompt
+  else if (lowercaseChoice === false && uppercaseChoice === false && numericChoice === false && specialChoice === false) {
+    alert("You must choose at least one password criteria");
+    var lowercaseChoice = confirm("Would you like your password to contain Lowercase letters?");
+    var uppercaseChoice = confirm("Would you like your password to contain Uppercase letters?");
+    var numericChoice = confirm("Would you like your password to contain numbers?");
+    var specialChoice = confirm("Would you like your password to contain special characters?");
+  }
+
+  // Use the criteria that are selected to generate the password from character options
+  if (lowercaseChoice) {
+    options += lowercaseChar;
+  }
+
+  if (uppercaseChoice) {
+    options += uppercaseChar;
+  }
+
+  if (numericChoice) {
+    options += numericChar;
+  }
+
+  if (specialChoice) {
+    options += specialChar;
+  }
+  
+  var randomPassword= ""
+  for (var i = 0; i < lengthChoice; i++) {
+    // Continues to select a random character value from the string until it is the desired length
+    randomString = [Math.floor(Math.random() * options.length)];
+    randomPassword = randomPassword + options[randomString]
+  }
+  return randomPassword;
+};
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  console.log("hello")
+  console.log("Random Password has been generated successfully!")
   passwordText.value = password;
-
 }
 
 // Everytime the generate password button is clicked, the writePassword function is run
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# passwordGenerator
-hello
+# Password Generator
+
+## Project Description 
+
+This project involved modifying a Password Generator that enables employees to generate random passwords based on criteria that they've selected. 
+
+Below is a summary of the changes/additions that have been made to Javascript file.
+
+* Function that generates a random passowrd based on criteria selected for the characters. 
+
+* "Generate Password" Button turns black once hovered over and stays black if clicked.
+
+* Validation prompts that allow users to determine their password criteria:
+
+*     Password length must be between 8 and 128 characters. 
+*     At least one from the following must be selected for the characters; lowercase letters, uppercase letters numeric values and special characters. 
+
+* Validation alerts when the above criteria are not met and a prompt to re-select.
+
+* Displayed password is cleared once the "Generate Password" button is clicked again.
+
+# Usage
+
+1. In order to run the application, click on the deployed application link.
+
+2. Once you have been redirected to the web-page, click on the red "Generate Password" button.
+
+3. You will be prompted with a series of questions regarding your password criteria (note, at least one must be selected, and the password length must be between 8 and 128 characters.)
+
+4. When all prompts are answered, you will be provided with a password on your screen which is generated according to your responses.
+
+5. The password will then clear when you press the "Generate Password" button again.
+
+![Website Screenshot](passwordgeneratorscreenshot.png)
+
+# Links
+[Click here to access the deployed website.](https://fadumaabdi.github.io/passwordGenerator/)
+
+[Click here to access the files on the github repository.](https://github.com/fadumaabdi/passwordGenerator)
+
